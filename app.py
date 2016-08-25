d945a0fede09924325142d0148c6896eeab25963
Edward Kim <edward@kobecreations.com>
2016-08-25 11:51:54 +1000

    initial commit

---
+++ app.py
@@ -0,0 +1,33 @@
+import socketio
+import eventlet
+from flask import Flask, render_template
+
+sio = socketio.Server()
+app = Flask(__name__)
+
+messages = []
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@sio.on('connect')
+def connect(sid, env):
+    print('connected %s' % sid)
+
+@sio.on('send message')
+def get_message(sid, data):
+    sio.emit('new message', {
+        "nickname": data["nickname"],
+        "body": data["body"]
+    }, skip_sid=sid)
+
+@sio.on('disconnect')
+def disconnect(sid):
+    print('disconnected %s' % sid)
+
+if __name__ == '__main__':
+    sioApp = socketio.Middleware(sio, app)
+
+    eventlet.wsgi.server(eventlet.listen(('', 8000)), sioApp)
+
